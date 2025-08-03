9a39387fa91c8d626e6f0d0dd66e489e57ce0ced
ramys <ramys@DESKTOP-RB8ABUH>
2025-08-03 16:51:43 +0300

    Add project files.

---
+++ FirstLecture/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstLecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"no really! {num} is my favorite number");
+            Int64[] numbers = [100, 2000, 3000000000];
+        }
+    }
+}
---
+++ MainTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MainTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of small carpets: ");
+            int SmallCarpet = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            int LargeCarpet = Convert.ToInt32(Console.ReadLine());
+            int TotalPribe = SmallCarpet * 25 + LargeCarpet * 35;
+            double TotalWithTax = TotalPribe + TotalPribe * 0.06;
+            Console.WriteLine($"Total estimate: ${TotalWithTax}");
+
+        }
+    }
+}
